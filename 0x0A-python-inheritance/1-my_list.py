dbe46675aac48549bf711898014c81ff10383b21
Chimaobi Ubani <wiztherealist@gmail.com>
2024-04-17 05:35:58 -0700

    lists

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+contains the MyList class
+"""
+
+
+class MyList(list):
+    """subclass of list"""
+    
+    def __init__(self):
+        """initializes object"""
+        super().__init__()
+
+    
+    def print_sorted(self):
+        """prints sorted list"""
+        print(sorted(self))
