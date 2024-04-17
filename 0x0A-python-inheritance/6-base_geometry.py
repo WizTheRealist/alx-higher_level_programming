1c71e2a3d19356b2ce76e27ccd3e7c2dd76d59f0
Chimaobi Ubani <wiztherealist@gmail.com>
2024-04-17 06:06:06 -0700

    inherits from

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""BaseGeometry
+
+Raises:
+    Exception: returns a joke
+"""
+
+
+class BaseGeometry:
+    """only one public instance method available
+    """
+
+
+    def area(self):
+        """returns  joke not implemented
+
+        Raises:
+            Exception: joke
+        """
+        
+        raise Exception("area() is not implemented")
