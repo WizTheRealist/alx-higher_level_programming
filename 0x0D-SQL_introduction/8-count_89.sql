6097ba0c39cadbc8f76ef4d8cd7cf888c0da9ba7
Chimaobi Ubani <Wiztherealist@gmail.com>
2024-05-24 03:47:22 +0100

    updated 8&12 of 0x0E

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -2,4 +2,4 @@
 -- in the table `first_table` of the database
 -- `hbtn_0c_0` in MySQL Server.
 
-SELECT COUNT(id) FROM `first_table` WHERE `id` = `89`;
+SELECT COUNT(id) FROM `first_table` WHERE `id` = '89';
